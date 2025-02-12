e824be5433d61f407957c721901b0e1cf93e5683
barbyns <barbyns95@gmail.com>
2025-02-12 16:39:27 +0100

    Update D3.js

--- D3.js
+++ D3.js
@@ -197,7 +197,8 @@ console.log("end")
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
   Una volta fatto crea un console.log per controllare la proprietà length di "charactersNames" prima e dopo l'operazione.
 */
-delete starWarsCharacters.name
+
+while (sstarWarsCharacters < 5)
 for (let i = 0; i < starWarsCharacters.length; i++){
   if(charactersNames[i].charAt(0) === "Luke") {
   }
